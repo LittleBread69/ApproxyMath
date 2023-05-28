e82addd458cd22834da8e88d56e0ec0e6788f8a5
LittleBread69 <111202841+LittleBread69@users.noreply.github.com>
2023-05-29 01:16:09 +0200

    Fixed apxsqrt.

--- SquareRoot/SqrtApprox.py
+++ SquareRoot/SqrtApprox.py
@@ -7,16 +7,17 @@ def apxsqrt(x: int):
         if x ** 0.5 % 1 == 0:
             return int(x ** 0.5)
         else:
+            y = x
             w = 0
-            while w ** 2 < x:
+            while w ** 2 < y:
                 w += 1
-            if w ** 2 > x:
+            if w ** 2 > y:
                 w -= 1
-            x = w + (x - w ** 2) / (2 * w)
-            if x ** 2 == (w + 1) ** 2:
-                return x - 0.05
+            y = w + (x - w ** 2) / (2 * w)
+            if y ** 2 == (w + 1) ** 2:
+                return y - 0.06
             else:             
-                return x
+                return y
 
 def prcsqrt(x: float):
     if x < 0:
