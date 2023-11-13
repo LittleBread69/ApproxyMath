b4dc125088a13446849f8082744836f6ddbedb60
LittleBread69 <111202841+LittleBread69@users.noreply.github.com>
2023-11-13 19:33:46 +0100

    fix

--- ApproxyMath.py
+++ ApproxyMath.py
@@ -20,8 +20,6 @@ from Fibonacci.fibonacci import *
 from Specific_Equations.xrootx import *
 from Trigonometry.cos_sin_tan import *
 
-lnpx = 0.6137056388802 #ln(x) + lnpx
-
 def help():
     print("ApproxyMath is small and polyvalent math (and other) approximation library.")
     print("Type 'Commands()' to get a list of all the commands and what they do.")
