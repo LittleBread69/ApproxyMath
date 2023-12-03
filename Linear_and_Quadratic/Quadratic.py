52819cca4321291dc0afa0843e6bb7e2b4a214b8
LittleBread69 <chelouminatis@gmail.com>
2023-12-03 23:50:36 +0100

    Create Quadratic.py

---
+++ Linear_and_Quadratic/Quadratic.py
@@ -0,0 +1,21 @@
+import math
+
+def Viete(a, b, c) -> tuple[int, int]|None:
+    delta = b * b - 4 * a * c
+    if delta >= 0:
+        return (-b - math.sqrt(delta)) / (2 * a), (-b + math.sqrt(delta)) / (2 * a)
+    return None
+
+def SimpleViete(b, c) -> tuple[int, int]|None:
+    delta = b * b - 4 * c
+    if delta >= 0:
+        return (-b - math.sqrt(delta)) / 2, (-b + math.sqrt(delta)) / 2
+    return None
+
+def Muller(a, b, c, postive=True)-> tuple[int, int]|None:
+    delta = b * b - 4 * a * c
+    if delta >= 0:
+        if positive:
+            return -2 * c / (b + math.sqrt(delta)), -2 * c / (b - math.sqrt(delta))
+        return 2 * c / (- b - math.sqrt(delta)), 2 * c / (b + math.sqrt(delta))
+    return None
