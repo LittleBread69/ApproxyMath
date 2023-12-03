672409ed775af8ccf997af7ad884a908de493fba
LittleBread69 <111202841+LittleBread69@users.noreply.github.com>
2023-12-04 00:28:29 +0100

    Fixed the quadratic file.

--- Linear_and_Quadratic/Quadratic.py
+++ Linear_and_Quadratic/Quadratic.py
@@ -1,21 +1,25 @@
 import math
+from functools import lru_cache as lcache
 
+@lcache()
 def Viete(a, b, c) -> tuple[int, int]|None:
     delta = b * b - 4 * a * c
     if delta >= 0:
         return (-b - math.sqrt(delta)) / (2 * a), (-b + math.sqrt(delta)) / (2 * a)
     return None
 
+@lcache()
 def SimpleViete(b, c) -> tuple[int, int]|None:
     delta = b * b - 4 * c
     if delta >= 0:
         return (-b - math.sqrt(delta)) / 2, (-b + math.sqrt(delta)) / 2
     return None
 
-def Muller(a, b, c, postive=True)-> tuple[int, int]|None:
+@lcache()
+def Muller(a, b, c, positive=True)-> tuple[int, int]|None:
     delta = b * b - 4 * a * c
     if delta >= 0:
         if positive:
-            return -2 * c / (b + math.sqrt(delta)), -2 * c / (b - math.sqrt(delta))
-        return 2 * c / (- b - math.sqrt(delta)), 2 * c / (b + math.sqrt(delta))
+            return -2 * c / (b - math.sqrt(delta)), -2 * c / (b + math.sqrt(delta))
+        return 2 * c / (- b + math.sqrt(delta)), 2 * c / (-b - math.sqrt(delta))
     return None
