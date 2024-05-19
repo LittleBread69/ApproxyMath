59aef64b3967b73e05d80aa2c2d982887de974bf
LittleBread69 <111202841+LittleBread69@users.noreply.github.com>
2024-05-19 17:00:07 +0200

    Works - Factorial.py updated

--- Factorial/Factorial.py
+++ Factorial/Factorial.py
@@ -1,10 +1,21 @@
+from functools import lru_cache
+import sys
 
-def fact(x:int|float) -> int|float:
-    if type(x) != int and type(x) != float: raise TypeError("Not accepted")
-    isFloat = False if type(x) == int else True
-    if x < 1.0: return x
-    result:float|int = 1.0
-    while x > 1:
-        result *= x
-        x -= 1
+@lru_cache(512)
+def fact(x: int | float) -> int | float:
+    if type(x) not in [int, float]:
+        raise TypeError("Not accepted")
+    if x < 0:
+        raise ValueError("Factorial is not defined for negative numbers")
+    isFloat = isinstance(x, float)
+    if x <= 1.0:
+        return x if isFloat else 1
+
+    result = x * fact(x - 1)
     return float(result) if isFloat else int(result)
+
+if __name__ == '__main__':
+    print(fact(2))
+    print(fact(2.0))
+    print(fact(0.3))
+    print(fact(22))
