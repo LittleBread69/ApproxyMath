b10dddd95c4ae44aa3e4020e350bfdc045bf5da1
LittleBread69 <chelouminatis@gmail.com>
2024-09-05 15:49:48 +0200

    Update Quadratic.py

--- Linear_and_Quadratic/Quadratic.py
+++ Linear_and_Quadratic/Quadratic.py
@@ -1,22 +1,22 @@
 import math
-from functools import lru_cache as lcache
+from functools import cache
 
-@lcache()
-def Viete(a, b, c) -> tuple[int, int]|None:
+@cache
+def Viete(a, b, c) -> tuple[int|float, int|float]|None:
     delta = b * b - 4 * a * c
     if delta >= 0:
         return (-b - math.sqrt(delta)) / (2 * a), (-b + math.sqrt(delta)) / (2 * a)
     return None
 
-@lcache()
-def SimpleViete(b, c) -> tuple[int, int]|None:
+@cache
+def SimpleViete(b, c) -> tuple[int|float, int|float]|None:
     delta = b * b - 4 * c
     if delta >= 0:
         return (-b - math.sqrt(delta)) / 2, (-b + math.sqrt(delta)) / 2
     return None
 
-@lcache()
-def Muller(a, b, c, positive=True)-> tuple[int, int]|None:
+@cache
+def Muller(a, b, c, positive=True)-> tuple[int|float, int|float]|None:
     delta = b * b - 4 * a * c
     if delta >= 0:
         if positive:
