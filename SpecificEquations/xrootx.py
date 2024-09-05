11e8b82fee84946deaac9583d1d2044b5ba1336b
LittleBread69 <chelouminatis@gmail.com>
2024-09-05 14:13:32 +0000

    fixed the type issue

--- SpecificEquations/xrootx.py
+++ SpecificEquations/xrootx.py
@@ -1,9 +1,9 @@
 from functools import cache
 
 @cache
-def xrootx(x:float|int|str) -> float|int:
+def xrootx(x:float|int|str) -> float|int|None: #type:ignore
     if x == 'max':
         return xrootx(2.718)
-    elif type(x) != str:
-        return x ** (1/x)
-    
+    elif type(x) in [int, float]:
+        return (x ** (1/x)) #type:ignore
+    return
