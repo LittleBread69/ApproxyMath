3e4a28c555cbd47d4a1202cc89907e211d1b462d
LittleBread69 <chelouminatis@gmail.com>
2024-09-05 15:56:23 +0200

    Update VariationMap.py

--- Utilities/RGB/VariationMap.py
+++ Utilities/RGB/VariationMap.py
@@ -1,5 +1,4 @@
-from numpy import sin, pi, sqrt
-import numpy as np
+from math import sin, pi, sqrt
 
 def VariatePixelColour(x:float) -> float:
     return ((((0.28846153846153844 * (sin(sqrt(x))) ** 3) * (12 / sin(sqrt(x))) * sin(6 * x) - sin(2 * x) + 17.5) ** 0.6766696666435431) / 8)
